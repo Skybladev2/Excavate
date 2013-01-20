a5543aadf3cd6697145d55f5fa3fd60a1f83aa11
Skyladev2 <qlzp2@mail.ru>
2013-01-20 21:58:12 +0400

    Added some script to excavator base, but it's dimensions are wrong (too small). Looks like meters from sketchup converted to inches (2.54 meter = 1 inch) then inches treated like meters (1 inch = 1 meter)

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -11,6 +11,26 @@ public class Controls : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		Debug.Log("R4P1 " +  Input.GetButton("R4P1").ToString());
+		//Debug.Log("X axis " + Input.GetAxis("HorizontalP1").ToString());
+		
+		
+	}
+	
+	void FixedUpdate()
+	{
+			if(Input.GetAxis("HorizontalP1") > 0)
+		{			
+			this.transform.RotateAround ( new Vector3(this.transform.position.x,
+													this.transform.position.y,
+													this.transform.position.z),
+										new Vector3(0,1, 0),
+										1f);
+		}
+		
+		if(Input.GetAxis("HorizontalP1") < 0)
+		{
+			this.transform.Translate(-0.01f, 0, 0);
+		}
+	
 	}
 }
